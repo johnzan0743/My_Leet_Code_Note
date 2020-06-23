1599018052580ee55b73b6d79912a999262658d5
johnzan0743 <65598659+johnzan0743@users.noreply.github.com>
2020-06-23 18:16:15 +1000

    1

---
+++ 1-双指针-Two Sum.py
@@ -0,0 +1,21 @@
+class Solution:
+    def twoSum(self, nums, target):
+        dic1 = {}
+        origin_index = list(range(len(nums)))
+        sorted_index = numpy.argsort(nums)
+        nums.sort()
+        for i in range(len(origin_index)):
+            dic1[sorted_index[i]] = origin_index[i]
+        i = 0
+        j = len(nums) - 1
+        while i < j:
+            sum = nums[i] + nums[j]
+            if sum == target:
+                result = [i ,j]
+                break
+            elif sum < target:
+                i += 1
+            else:
+                j -= 1
+        actual_index = [sorted_index[i], sorted_index[j]]
+        return actual_index
