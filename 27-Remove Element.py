13f059a97bc0ada8da4cd1c960146e3449c7b4e8
johnzan0743 <65598659+johnzan0743@users.noreply.github.com>
2020-06-27 10:17:30 +1000

    Update 27-Remove Element.py

--- 27-Remove Element.py
+++ 27-Remove Element.py
@@ -9,11 +9,31 @@ class Solution:
                 count +=1
         return count
 # 注意：此处的count是指length，而不是index，所以return count不需要减一
-        
-#         count = 0
-#         for i in range(len(nums)):
-#             if nums[i] != val:
-#                 nums[count] = nums[i]
-#                 count +=1
-#         return count
+
+#双指针 
+'''
+跟方法一类似，这里也要注意，position指的是index而非length
+'''
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        position = len(nums) -1
+        i = 0
+        while i <= position:
+            if nums[i] == val:
+                nums[i], nums[position] = nums[position], nums[i]
+                position -=1
+            else:
+                i +=1
+        return position +1
+                
+
+
+#暴力法
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        while nums.count(val)>0:
+            nums.remove(val)
+        return len(nums)
+
+
                 
