e956d92b28d76669cc21e892692b83011b5a21a6
johnzan0743 <65598659+johnzan0743@users.noreply.github.com>
2020-07-13 23:49:17 +1000

    Update 206-Reverse Linked List.py

--- 206-Reverse Linked List.py
+++ 206-Reverse Linked List.py
@@ -2,9 +2,19 @@ class Solution(object):
     def reverseList(self, head):
         previous = None
         current = head
+        # （0） -> 1 -> 2
+        #  pre   curr temp
         while current:
             temp = current.next
+            # (0) -> 1 -> 2
             current.next = previous
+            # 1 -> (0).....2 -> 3
             previous = current
             current = temp
+            # pre : 1 
+            # current: 2
+            # temp = 3
         return previous
+# 在linked list中，当 X.next处于等号左边时，表示（重新）定义X node的next指针方向
+# 当 Y.next处于等号右边时，表示把Y node的next指针方向赋予给某node 或某node的next指针
+# 或移动Y node：Y = Y.next
